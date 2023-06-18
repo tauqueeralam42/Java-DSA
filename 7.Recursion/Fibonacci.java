07326e1280e1ee8d81744d91f2e1063b8ebb1482
Tauqueer Alam <tauqueeralam42@gmail.com>
2023-06-18 12:49:40 +0530

    Recursion

---
+++ 7.Recursion/Fibonacci.java
@@ -0,0 +1,15 @@
+public class Fibonacci {
+    
+    public static int fib(int n){
+        if(n==1)
+            return 1;
+        else if(n==0)
+            return 0;
+        return fib(n-1)+fib(n-2);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fib(25));
+    }
+}
