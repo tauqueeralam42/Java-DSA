014866d8c2343062d62c67a8defd22af4abd89b3
Tauqueer Alam <tauqueeralam42@gmail.com>
2023-08-07 08:15:13 +0530

    DP

--- 21.DP/MinArrayJump.java
+++ 21.DP/MinArrayJump.java
@@ -23,6 +23,8 @@ public class MinArrayJump {
     }
     
     public static void main(String[] args) {
+
+        
         int arr[] = {2,3,1,1,4};
         System.out.println(minJump(arr));
     }
